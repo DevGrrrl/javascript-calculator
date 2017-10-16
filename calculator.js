8e19b0eedfc4b01aadab494720be4779ca0e6412
Lex <alexisjaynedefoe@gmail.com>
2017-10-16 14:11:41 +0100

    do nothing if operator is pressed more than once at a time

--- calculator.js
+++ calculator.js
@@ -23,8 +23,10 @@ var ac = document.querySelector('#ac');
 
 
 //Button Styling when clicked
-var keysArray = [zero, one, two, three, four, five, six, seven, eight, nine,  minus, dot, percent, times, plus,  equals, ac, ce,];
+var keysArray = [zero, one, two, three, four, five, six, seven, eight, nine,  minus, percent, times, plus, dot, equals, ac, ce,];
 
+var displayCount = 0;
+var operator = /\+|\=|\*|\/|\-/;
 
 var valZero = zero;
 
@@ -49,6 +51,8 @@ var calc = [];
 var total = 0;
 var displayValue = document.getElementById('display-value');
 var displayCalculation = document.getElementById('display-calculation');
+var displayMax;
+var valueArrayLength = 0;
 
 displayValue.textContent = 0;
 
@@ -57,23 +61,70 @@ displayCalculation.textContent = 0;
 
 function pushToArray(e){
 
+ displayCount += 1;
+
+ if (displayCount > 8){
+  if (this !== /\+|\=|\*|\/|\-/){
+  displayValue.textContent = 0;
+  displayCalculation.textContent = "Digit Limit Met";
+  displayCount = 0;
+  valueArray =[];
+  console.log(displayCount);
+}}
+
+ else {
   valueArray.push(this.value);
   console.log(this.value);
-
   displayValue.textContent = this.value;
   displayCalculation.textContent = valueArray.join('');
-
-
-
-   console.log(valueArray);
+  console.log(displayCount);
+  displayMax = (valueArray.join('')).length;
+  valueArrayLength = valueArray.length;
 }
 
+}
 
 function operatorPush(e){
 
-  if (valueArray.length === 0) {
-    return}
-  else
+  if (valueArray.length === 0 ) {
+    if (this.value !== '-'){
+        return}
+    }
+
+  if (valueArray[valueArrayLength-1] === this.value) {
+    return;
+  }
+
+  if
+  // (valueArray.join('').charAt(valueArrayLength-1).match(operator))
+
+    (valueArray.join('').charAt(((valueArray.join('')).length)-1).match(operator))
+
+   {
+
+    console.log(valueArray);
+    return;
+  }
+
+    displayCount =0;
+    valueArray.push(this.value);
+    console.log(this.value);
+    displayValue.textContent = this.value;
+    displayCalculation.textContent = valueArray.join('');
+    console.log(valueArray);
+    valueArrayLength = valueArray.length;
+
+}
+
+//decimal point//
+
+function dotPush(e){
+
+  //Why not valueArray[valueArrayLength-1]?
+  if (valueArray[valueArrayLength] === this.value){
+    return;
+  }
+  else {
     valueArray.push(this.value);
     console.log(this.value);
 
@@ -81,21 +132,38 @@ function operatorPush(e){
     displayCalculation.textContent = valueArray.join('');
     console.log(valueArray);
   }
+}
+
 //Equals
 function sumArray(e){
 
 //***** ONLY SUM IF CONTAINS A NUMBER*****
+
+
   if (valueArray.length === 0){
     return;
   }
 
-  else
+
+
   total = eval(valueArray.join(""));
+
+  if (total
+    === Infinity){
+
+        displayCalculation.textContent = "error";
+        valueArray = [];
+        displayValue.textContent = "error";
+        total === 0;
+      }
+
+  else {
+
   displayCalculation.textContent = valueArray.join('')+ "=" +total;
   valueArray = [total];
   displayValue.textContent = total;
+  }
 
-  console.log(total);
 }
 
 
@@ -104,34 +172,40 @@ function clearAll(e){
  total = 0;
  displayValue.textContent = 0;
  displayCalculation.textContent = 0;
+ displayCount = 0;
+ valueArrayLength = 0;
  console.log(valueArray);
  console.log(total);
 }
 
 function clearLast(e){
+  displayCount = 0;
   valueArray.pop();
+  valueArrayLength = valueArray.length;
   if (valueArray.length === 0){
     displayValue.textContent = 0;
     displayCalculation.textContent = 0;
   }
    else {displayCalculation.textContent = valueArray.join('')};
 
+
   console.log(valueArray);
   console.log(total);
 }
 //Push key value to array
-for (var i = 0; i <12; i ++){
+for (var i = 0; i <10; i ++){
 
 keysArray[i].addEventListener("click", pushToArray);
 }
 
-for (var i = 12; i <15; i ++){
+for (var i = 10; i <14; i ++){
 
 keysArray[i].addEventListener("click", operatorPush);
 }
 
-// 
-// dot.addEventListener("click", dotPush);
+
+
+dot.addEventListener("click", dotPush);
 
 //Evaluate array once equals is pushed
 equals.addEventListener("click", sumArray);
