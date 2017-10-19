b5638f5aa5cc818cb1d8e4099c06c94c7b82e90b
Lex <alexisjaynedefoe@gmail.com>
2017-10-19 17:09:54 +0100

    Limit number of leading zero's before decimal place to 1. Changed some ID's and variable names to pass FCC testing suite. Sum numbers toFixed(4). Set display max for solution to 9.

--- calculator.js
+++ calculator.js
@@ -8,19 +8,19 @@ var seven = document.querySelector('#seven');
 var eight = document.querySelector('#eight');
 var nine = document.querySelector('#nine');
 
-var minus = document.querySelector('#minus');
-var percent = document.querySelector('#percent');
-var times = document.querySelector('#times');
-var plus = document.querySelector('#plus');
-var dot = document.querySelector('#dot');
+var subtract = document.querySelector('#subtract');
+var divide = document.querySelector('#divide');
+var multiply = document.querySelector('#multiply');
+var add = document.querySelector('#add');
+var decimal = document.querySelector('#decimal');
 
 var equals = document.querySelector('#equals');
-var ac = document.querySelector('#ac');
+var clear = document.querySelector('#clear');
 var ce = document.querySelector('#ce');
 
 
 //Button Styling when clicked
-var keysArray = [zero, one, two, three, four, five, six, seven, eight, nine,  minus, percent, times, plus, dot, equals, ac, ce,];
+var keysArray = [zero, one, two, three, four, five, six, seven, eight, nine,  subtract, divide, multiply, add, decimal, equals, clear, ce,];
 
 var displayCount = 0;
 var operator = /\+|\=|\*|\/|\-/;
@@ -40,15 +40,16 @@ function buttonUp() {
 for (var i = 0; i <18; i ++){
  keysArray[i].addEventListener("mousedown", buttonDown);
  keysArray[i].addEventListener("mouseup", buttonUp);
-  console.log(keysArray[i]);
+  // console.log(keysArray[i]);
 }
 //button styling end
 
 //Calculations
+var newNumber =[];
 var valueArray = [];
 var calc = [];
 var total = 0;
-var displayValue = document.getElementById('display-value');
+var displayValue = document.getElementById('display');
 var displayCalculation = document.getElementById('display-calculation');
 var displayMax;
 var valueArrayLength = 0;
@@ -59,49 +60,80 @@ displayCalculation.textContent = 0;
 
 
 function pushToArray(e){
-
- displayCount += 1;
  operatorTrack = 0;
+ displayCount += 1;
 
- if (displayCount > 8){
-  if (this !== /\+|\=|\*|\/|\-/){
-  displayValue.textContent = 0;
-  displayCalculation.textContent = "Digit Limit Met";
-  displayCount = 0;
-  valueArray =[];
+if (displayCount > 8){
+    if (this !== /\+|\=|\*|\/|\-/){
+    displayValue.textContent = 0;
+    displayCalculation.textContent = "Digit Limit Met";
+    displayCount = 0;
+    displayArr = [];
+    valueArray =[];
+    newNumber = [];
+    }}
 
+       else if (equalsTrack === 1){
+         equalsTrack = 0;
+         valueArray = [this.value];
+         displayArr = [this.value];
+         displayValue.textContent = valueArray;
+         displayCalculation.textContent = this.value;
+         newNumber = [];
+        }
 
-}}
+          else if (this.value === '0'){
+             if(valueArrayLength === 0){
+               displayCount = 0;
+               return;
+             }
 
- else if (equalsTrack === 1){
-   equalsTrack = 0;
-   valueArray = [this.value];
-   displayArr = [this.value];
-   displayValue.textContent = valueArray;
-   displayCalculation.textContent = this.value;
+if (newNumber[0] === "0"){
+       if(newNumber[1] !== "."){
+         displayCount -=1;
+         return;
+       }
+         else
+
+         newNumber.push(this.value);
+         equalsTrack = 0;
+         valueArray.push(this.value);
+         displayArr.push(this.value);
+         displayValue.textContent = displayArr.join('');
+         displayCalculation.textContent = valueArray.join('');
+         displayMax = (valueArray.join('')).length;
+         valueArrayLength = valueArray.length;
+       }
+
+    else {
+      newNumber.push(this.value);
+      equalsTrack = 0;
+      valueArray.push(this.value);
+      displayArr.push(this.value);
+      displayValue.textContent = displayArr.join('');
+      displayCalculation.textContent = valueArray.join('');
+      displayMax = (valueArray.join('')).length;
+      valueArrayLength = valueArray.length;
+      }
 }
 
-
- else {
-  equalsTrack = 0;
-  valueArray.push(this.value);
-  displayArr.push(this.value);
-  displayValue.textContent = displayArr.join('');
-  // displayValue.textContent = this.value;
-  displayCalculation.textContent = valueArray.join('');
-  displayMax = (valueArray.join('')).length;
-  valueArrayLength = valueArray.length;
-  console.log(valueArray);
-  console.log(total);
-  console.log(displayArr);
-}
-
+  else {
+    newNumber.push(this.value);
+    equalsTrack = 0;
+    valueArray.push(this.value);
+    displayArr.push(this.value);
+    displayValue.textContent = displayArr.join('');
+    displayCalculation.textContent = valueArray.join('');
+    displayMax = (valueArray.join('')).length;
+    valueArrayLength = valueArray.length;
+  }
 }
 
 function operatorPush(e){
 
   equalsTrack = 0;
   operatorTrack = 1;
+  newNumber = [];
 
   if (valueArray.length === 0 ) {
     if (this.value === '-'){
@@ -112,21 +144,20 @@ function operatorPush(e){
      displayCalculation.textContent = valueArray.join('');
      valueArrayLength = valueArray.length;
     }
-     else if (this.value !== '-'){
-        return}
+       else if (this.value !== '-'){
+                return
+                }
     }
 
     //Can remove code below? Dupes line 98?//
 
-  if (valueArray[valueArrayLength-1] === this.value) {
+if (valueArray[valueArrayLength-1] === this.value) {
     return;
     }
 
-
-  if
+if
 // if an operator is pushed more than once at a time //
    (valueArray.join('').charAt(((valueArray.join('')).length)-1).match(operator))
-
       {
         console.log(valueArray);
         console.log(total);
@@ -134,23 +165,21 @@ function operatorPush(e){
       return;
       }
 
- else {
+   else {
 
-    displayCount = 0;
-    valueArray.push(this.value);
-    displayValue.textContent = this.value;
-    displayCalculation.textContent = valueArray.join('');
-    displayArr = [];
-    valueArrayLength = valueArray.length;
-    console.log(valueArray);
-    console.log(total);
-    console.log(displayArr);
+      displayCount = 0;
+      valueArray.push(this.value);
+      displayValue.textContent = this.value;
+      displayCalculation.textContent = valueArray.join('');
+      displayArr = [];
+      valueArrayLength = valueArray.length;
+      }
 }
-}
+
 
 //decimal point//
 
-function dotPush(e){
+function decimalPush(e){
 
   var dec = [];
   var slice;
@@ -165,28 +194,29 @@ function dotPush(e){
       dec.push(i);
     }
   }
+
   slice = valueArray.slice(-dec[0]);
 
   if (slice.indexOf(".") !== -1){
     return
-  }
+    }
 
   else {
+    newNumber.push(this.value);
     valueArray.push(this.value);
+    valueArrayLength = valueArray.length;
     displayCount += 1;
     displayArr.push(this.value);
     //add preceeding digitst to displayValue;
     displayValue.textContent = this.value;
     displayCalculation.textContent = valueArray.join('');
-    console.log(valueArray);
-    console.log(total);
-    console.log(displayArr);
-  }
+    }
 }
 
-//Equals
-function sumArray(e){
 
+
+//SUM
+function sumArray(e){
 
 //***** ONLY SUM IF CONTAINS A NUMBER or last value is a number*****
 
@@ -197,74 +227,78 @@ function sumArray(e){
    else if (valueArray[valueArrayLength-1] === "+" ||
             valueArray[valueArrayLength-1] === "-" ||
             valueArray[valueArrayLength-1] === "/" ||
-            valueArray[valueArrayLength-1] === "*"){
-
-    return;
-  }
-
-
+            valueArray[valueArrayLength-1] === "*" ||
+            valueArray[valueArrayLength-1] === ".")
+            {return;
+            }
 
 
-  if (total === Infinity){
+
+
+if (total === Infinity){
         displayCalculation.textContent = "error";
         valueArray = [];
         displayArr = [];
         displayValue.textContent = "error";
         total === 0;
+        newNumber = [];
         }
 
-  if (total.toString().length >8) {
-            displayValue.textContent = 0;
-            displayCalculation.textContent = "Digit Limit Met";
-            displayCount = 0;
-            valueArray =[];
-            displayArr = [];
-            }
-  else  {
-            equalsTrack = 1;
-            total = (eval(valueArray.join("")));
-            displayCalculation.textContent = valueArray.join('')+ "=" +total;
-            valueArray = [total];
-            displayArr = [total];
-            displayValue.textContent = total;
+        else  {
+              newNumber = [];
+              equalsTrack = 1;
+              total = (eval(valueArray.join("")).toFixed(4))/1;
 
-            }
+                if (total.toString().length > 9){
+
+                  displayValue.textContent = 0;
+                  displayCalculation.textContent = "Digit Limit Met";
+                  displayCount = 0;
+                  displayArr = [];
+                  valueArray =[];
+                  newNumber = [];
+
+                }
+                    else {
+                    displayCalculation.textContent = valueArray.join('')+ "=" +total;
+                    valueArray = [total];
+                    displayArr = [total];
+                    displayValue.textContent = total;
+                    }
+    }
 
 }
-
-
 
 function clearAll(e){
- valueArray = [];
- equalsTrack = 0;
- total = 0;
- displayValue.textContent = 0;
- displayCalculation.textContent = 0;
- displayCount = 0;
- valueArrayLength = 0;
- displayArr = [];
- console.log(valueArray);
- console.log(total);
- console.log(displayArr);
-}
+
+     valueArray = [];
+     equalsTrack = 0;
+     total = 0;
+     displayValue.textContent = 0;
+     displayCalculation.textContent = 0;
+     displayCount = 0;
+     valueArrayLength = 0;
+     displayArr = [];
+     newNumber = [];
+      }
 
 function clearLast(e){
-  displayCount = 0;
-  valueArray.pop();
-  valueArrayLength = valueArray.length;
-  displayArr.pop();
+    displayCount = 0;
+    valueArray.pop();
+    valueArrayLength = valueArray.length;
+    displayArr.pop();
+    newNumber.pop();
 
-  if (valueArray.length === 0){
-    displayValue.textContent = 0;
-    displayCalculation.textContent = 0;
+    if (valueArray.length === 0){
+      displayValue.textContent = 0;
+      displayCalculation.textContent = 0;
+    }
+       else {
+         displayCalculation.textContent = valueArray.join('');
+       }
+
+     displayValue.textContent = 0;
   }
-   else {displayCalculation.textContent = valueArray.join('')};
-   displayValue.textContent = 0;
-
-   console.log(valueArray);
-   console.log(total);
-   console.log(displayArr);
-}
 
 
 //Push key value to array
@@ -278,38 +312,13 @@ for (var i = 10; i <14; i ++){
 keysArray[i].addEventListener("click", operatorPush);
 }
 
-
-
-
-//decimalPlace duplication
-//
-// (valueArray.join('').charAt(((valueArray.join('')).length)-1).match(operator))
-
-
-// return slice;
-
-// }
-
-
-
-dot.addEventListener("click", dotPush);
+decimal.addEventListener("click", decimalPush);
 
 //Evaluate array once equals is pushed
 equals.addEventListener("click", sumArray);
 
 //AC - clear All
-ac.addEventListener("click", clearAll);
+clear.addEventListener("click", clearAll);
 
 //CE - clear Last
 ce.addEventListener("click", clearLast);
-
-
-
-//Calculations end
-
-
-
-
-
-
-//Display values in screen//
